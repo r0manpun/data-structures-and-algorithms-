556de8ffc777b629ab750b4cdeee32ba8f778139
r0manpun <punroman112@gmail.com>
2025-06-16 14:06:03 +0545

    feat: solve first recurring character in an array

---
+++ hashTable/1_firstRecurringCharacter.js
@@ -0,0 +1,60 @@
+// Google Question
+// Given an array = [2,5,1,2,3,5,1,2,4]
+// It should return 2
+
+// Given an array = [2,1,1,2,3,5,1,2,4]
+// It should return 1
+
+// Given an array = [2,3,4,5]
+// It should return undefined
+
+// parameter - array
+// nested loop - O(n^2)
+// best approach - O(n)
+
+// Naive approach
+function firstRecurringCharacter(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        return arr[j];
+      }
+    }
+  }
+
+  // loop throgh the array
+  // if array at i = hashmap[i]
+  // retun hashmap
+  // undefined
+  return undefined;
+}
+
+// Hash Map
+function firstRecurringCharacter2(arr) {
+  const map = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] !== undefined) {
+      return arr[i];
+    } else {
+      map[arr[i]] = i;
+    }
+  }
+  return undefined;
+}
+
+// Hash Set
+function firstRecurringCharacter3(arr) {
+  const set = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (set.has(arr[i])) {
+      return arr[i];
+    } else {
+      set.add(arr[i]);
+    }
+  }
+  return undefined;
+}
+
+console.log(firstRecurringCharacter3([2, 5, 1, 2, 3, 5, 1, 2, 4])); // 2
+console.log(firstRecurringCharacter3([2, 1, 1, 2, 3, 5, 1, 2, 4])); // 1
+console.log(firstRecurringCharacter3([2, 3, 4, 5]));
